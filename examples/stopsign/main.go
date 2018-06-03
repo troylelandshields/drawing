fb470338e1102b8b1b9d621ae9f77851c06ddba0
Lindsay Shields <lindsayshields@tboss.local>
2018-06-03 17:29:05 -0600

    added DrawStopSign function and example

--- examples/stopsign/main.go
+++ examples/stopsign/main.go
@@ -15,19 +15,8 @@ func main() {
 	canvas.Move(drawing.Left, 4)
 	canvas.Turn(drawing.Right)
 
-	// drawing octagon
-	for i := 0; i < 8; i++ {
-		canvas.DrawLine(drawing.Red, 45, 3)
-	}
+	canvas.DrawStopSign(drawing.Red, drawing.Cyan)
 
-	// drawing pole
-	canvas.Move(drawing.Right, 4)
-	canvas.Move(drawing.Right, 5)
-	canvas.Turn(drawing.Left)
-
-	canvas.DrawRectangle(drawing.Cyan, 10, 1)
-
-	canvas.DrawLine(drawing.Green, drawing.Straight, 3)
 	// don't forget to save the canvas to a file
 	err := canvas.SaveImage("stopsign.png")
 	if err != nil {
