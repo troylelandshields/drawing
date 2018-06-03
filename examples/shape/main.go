459e3af96969ab29eecc2a8f2e9ba0547ee67509
Holly <hollyb3010@gmail.com>
2018-06-03 14:23:28 -0600

    holly-shape

---
+++ examples/shape/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/troylelandshields/drawing"
+)
+
+func main() {
+	// create a new canvas on which to draw
+	canvas := drawing.NewCanvas(1000, 1000)
+
+	// draw the other 4 lines
+	for i := 0; i < 5; i++ {
+
+		canvas.Move(0, 0)
+		canvas.DrawPoly(drawing.Magenta, 5)
+	}
+
+	// don't forget to save the canvas to a file
+	err := canvas.SaveImage("poly.png")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
